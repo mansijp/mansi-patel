5147985911577b93105616f809030f4658186c7a
Mansi Patel <45537538+mansijp@users.noreply.github.com>
2024-08-26 22:44:45 -0400

    Home page content

---
+++ src/components/Home/home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './home.css';
+import MansiPatel from '../../img/mansi-patel.png';
+
+const Home = () => {
+    return (
+        <section id="home">
+            <div className="home-content">
+                <span className="hello">Hello,</span><br/>
+                <span className="intro">I'm <span className="name">Mansi Patel</span></span><br/>
+                <hr className="line"/>
+                <p className="intro-text">
+                    I am a Software Engineer passionate about full-stack development,<br/>
+                    cloud computing, and mobile application development.
+                </p>
+            </div>
+            <img src={MansiPatel} alt="Mansi Patel" className="profile" />
+        </section>
+    )
+}
+
+export default Home;
