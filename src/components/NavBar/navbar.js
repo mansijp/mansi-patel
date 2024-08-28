63b672d05ea8be6f02472e323a67c24d01bd47a5
Patel <mansijpatel321@gmail.com>
2024-08-28 13:08:06 -0400

    Start to adding a darkness preference

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './navbar.css';
-import logo from '../../img/logo.png';
+import logo from '../../img/light-setting.png';
 import resume from '../../img/resume-black.png';
 import {Link} from 'react-scroll';
 
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <img src={logo} alt="Logo" className="logo" />
+            <img src={logo} alt="Light Setting" className="light" />
             <div className="menuItems">
                 <Link className="menuItem">Home</Link>
                 <Link className="menuItem">About</Link>
