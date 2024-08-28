a67e51b45d432fe0940d6eeee221b13663cb33a0
Patel <mansijpatel321@gmail.com>
2024-08-28 14:53:05 -0400

    Adding new sections' boilerplates

---
+++ src/components/Education/education.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './education.css';
+
+const Education = () => {
+    return(
+        <section id="education">
+            <div>Education</div>
+        </section>
+    )
+}
+
+export default Education;
