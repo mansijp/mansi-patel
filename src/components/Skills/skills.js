4f5aea41b013500a564ee4607030810ba3e4b83d
Patel <mansijpatel321@gmail.com>
2024-08-28 16:07:35 -0400

    Designed a skill component

--- src/components/Skills/skills.js
+++ src/components/Skills/skills.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './skills.css';
+import JS from '../../img/javascript.png';
 
 const Skills = () => {
     return(
         <section id="skills">
-            <div>skills</div>
+            <div className='skills-content'>
+                <div className='table-row-skills'>
+                    <div className='table-column-skills'>
+                        <span className='skill-name'><img className='skill-img' src={JS} /><p id="text">JavaScript</p></span>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
