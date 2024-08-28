c5d5da122812997814b5eed2adba3069e83ad068
Patel <mansijpatel321@gmail.com>
2024-08-28 15:10:44 -0400

    Added new section 'Achievements'

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import Navbar from "./components/NavBar/navbar";
 import Home from "./components/Home/home";
 import About from "./components/About/about";
 import Contact from "./components/Contact/contact";
+import Education from "./components/Education/education";
+import Experience from "./components/Experience/experience";
+import Skills from "./components/Skills/skills";
+import Achievements from './components/Achievements/achievements';
 
 function App() {
   return (
@@ -9,6 +13,10 @@ function App() {
         <Navbar/>
         <Home />
         <About />
+        <Skills />
+        <Experience />
+        <Achievements />
+        <Education />
         <Contact />
     </div>
   );
--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import './navbar.css';
-import logo from '../../img/light-setting.png';
+import light from '../../img/light-setting.png';
 import resume from '../../img/resume-black.png';
 import {Link} from 'react-scroll';
 
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <img src={logo} alt="Light Setting" className="light" />
+            <img src={light} alt="Light Setting" className="light" />
             <div className="menuItems">
                 <Link className="menuItem">Home</Link>
                 <Link className="menuItem">About</Link>
                 <Link className="menuItem">Skills</Link>
                 <Link className="menuItem">Experience</Link>
                 <Link className="menuItem">Projects</Link>
+                <Link className="menuItem">Achievements</Link>
                 <Link className="menuItem">Education</Link>
                 <Link className="menuItem">Contact</Link>
             </div>
