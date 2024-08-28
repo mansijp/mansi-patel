ef4d7c7d02f9e70bce8ca01210b17640fa7b5ab9
Patel <mansijpatel321@gmail.com>
2024-08-28 13:06:49 -0400

    Added Contact section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from "./components/NavBar/navbar";
 import Home from "./components/Home/home";
-import About from "./components/About/about"
+import About from "./components/About/about";
+import Contact from "./components/Contact/contact";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
         <Navbar/>
         <Home />
         <About />
+        <Contact />
     </div>
   );
 }
---
+++ src/components/Contact/contact.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './contact.css';
+import email from '../../img/email-icon.png';
+import linkedin from '../../img/linkedin-icon.png';
+import github from '../../img/github-icon.png';
+import devpost from '../../img/devpost-icon.png';
+
+const Contact = () => {
+    return (
+        <section id="contact">
+            <div className='contact-content'>
+                <div className='table-row-contact'>
+                    <div className='table-column-contact'><span className='title'>Get in Touch: </span></div>
+                    <div className='table-column-contact'>
+                        <a href="mailto:mansijpatel321@gmail.com"><img className='contact' src={email} alt="Email" /></a>
+                    </div>
+                    <div className='table-column-contact'>
+                        <a href="https://www.linkedin.com/in/mansijp/" target="_blank" rel="noreferrer"><img className='contact' src={linkedin} alt="LinkedIn" /></a>
+                    </div>
+                    <div className='table-column-contact'>
+                        <a href="https://github.com/mansijp" target="_blank" rel="noreferrer"><img className='contact' src={github} alt="GitHub" /></a>
+                    </div>
+                    <div className='table-column-contact'>
+                        <a href="https://devpost.com/mansijp" target="_blank" rel="noreferrer"><img className='contact' src={devpost} alt="Devpost" /></a>
+                    </div>
+                    <div className='table-column-contact'>
+                        <span className='copyright'>© Mansi Patel 2024</span>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Contact;
+
