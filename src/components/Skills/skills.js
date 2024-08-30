21f132a27614a06c78e28bd2e65a55f8a6523493
Patel <mansijpatel321@gmail.com>
2024-08-30 13:18:24 -0400

    Designing table component on Skills section

--- src/components/Skills/skills.js
+++ src/components/Skills/skills.js
@@ -5,10 +5,18 @@ import JS from '../../img/javascript.png';
 const Skills = () => {
     return(
         <section id="skills">
-            <div className='skills-content'>
+            <div className='table-skills'>
                 <div className='table-row-skills'>
                     <div className='table-column-skills'>
-                        <span className='skill-name'><img className='skill-img' src={JS} /><p id="text">JavaScript</p></span>
+                        <div className='skill-section'><span className='skill-section-text'></span>Programming Languages
+                            <span className='skill-name'>
+                                <img className='skill-img' src={JS} /><p id="text">JavaScript</p>
+                                <img className='skill-img' src={JS} /><p id="text">JavaScript</p>
+                                <img className='skill-img' src={JS} /><p id="text">JavaScript</p>
+                                <img className='skill-img' src={JS} /><p id="text">JavaScript</p>
+                                <img className='skill-img' src={JS} /><p id="text">JavaScript</p>
+                            </span>
+                        </div>
                     </div>
                 </div>
             </div>
